7beed1cfbf74000feb12689004ad4e7be0c23248
Stefan Bowerman <stefbowerman@gmail.com>
2014-10-01 16:00:40 -0700

    Changed the menu to a hamburger.  Added next / previous links to the posts.  Got rid of shuffling

--- wp-content/themes/elderstatesman/js/src/app.js
+++ wp-content/themes/elderstatesman/js/src/app.js
@@ -1,8 +1,36 @@
 ;(function($){
 
     //mobile nav
-    $(".nav-button").on('click', function() {
+    $(".mobile-nav-button").on('click', function() {
       $(".left-nav").find("ul.menu").slideToggle();
+    });
+
+    $('.desktop-nav-button').on('click', function(){
+      var $sidebarMenuContainer = $('.sidebar-menu-container');
+
+      var isOpen = ($sidebarMenuContainer.data('is-open') || false);
+
+      if(isOpen){
+        $sidebarMenuContainer.animate({
+          opacity: '0',
+          top: '-=10px'
+        }, 200, function(){
+          $sidebarMenuContainer.css({
+            'visibility': 'invisible'
+          }).data('is-open', !isOpen);
+        });
+      }
+      else {
+        $sidebarMenuContainer.css({
+          'visibility': 'visible'
+        }).animate({
+          opacity: '1',
+          top: '+=10px'
+        }, 200, function(){
+          $sidebarMenuContainer.data('is-open', !isOpen);
+        });
+      }
+
     });
 
 
